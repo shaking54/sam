e1a75b48d7f8aad30e039d237de63782ebd51806
shaking54 <trantrungnguyen154@gmail.com>
2024-03-23 22:40:03 +0700

    update

--- train_sam.bash
+++ train_sam.bash
@@ -2,17 +2,17 @@
 python train.py --arch wideres --optim sam --num_class 10 --epochs 100
 
 # resnet18 optim sam cifar100
-python train.py --arch wideres --optim sam --num_class 100 --epochs 100
+python train.py --arch wideres --optim sam --num_class 100 --epochs 200
 
 # resnet18 optim sam cifar10
 python train.py --arch res18 --optim sam --num_class 10 --epochs 100
 
 # resnet18 optim sam cifar100
-python train.py --arch res18 --optim sam --num_class 100 --epochs 100
+python train.py --arch res18 --optim sam --num_class 100 --epochs 200
 
 # vgg16 optim sam cifar10
 python train.py --arch vgg16 --optim sam --num_class 10 --epochs 100
 
 # vgg16 optim sam cifar100
-python train.py --arch vgg16 --optim sam --num_class 100 --epochs 100
+python train.py --arch vgg16 --optim sam --num_class 100 --epochs 200
 
--- train_sgd.bash
+++ train_sgd.bash
@@ -2,17 +2,17 @@
 python train.py --arch wideres --optim sgd --num_class 10 --epochs 100
 
 # resnet18 optim sam cifar100
-python train.py --arch wideres --optim sgd --num_class 100 --epochs 100
+python train.py --arch wideres --optim sgd --num_class 100 --epochs 200
 
 # resnet18 optim sgd cifar10
 python train.py --arch res18 --optim sgd --num_class 10 --epochs 100
 
 # resnet18 optim sgd cifar100
-python train.py --arch res18 --optim sgd --num_class 100 --epochs 100
+python train.py --arch res18 --optim sgd --num_class 100 --epochs 200
 
 # vgg16 optim sgd cifar10
 python train.py --arch vgg16 --optim sgd --num_class 10 --epochs 100
 
 # vgg16 optim sgd cifar100
-python train.py --arch vgg16 --optim sgd --num_class 100 --epochs 100
+python train.py --arch vgg16 --optim sgd --num_class 100 --epochs 200
 
